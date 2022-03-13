ac5c3444e1efdb785d0de74974377e5f1ad1310e
JokeDevil <49778541+JokeDevil@users.noreply.github.com>
2022-03-13 21:07:39 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -13,7 +13,7 @@
     #####################################################################
 ]]
 
-version '2.1.7.10'
+version '2.1.7.10b'
 author 'Prefech'
 description 'FXServer logs to Discord (https://prefech.com/)'
 repository 'https://github.com/prefech/JD_logs'
